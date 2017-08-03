5a9465680c5ac0c795ea03c2ba4dfd4fd65cee22
doniyor <doniyor2109@mail.ru>
2017-08-04 00:41:15 +0500

    multiple users in seeds

--- test/seeds/user.js
+++ test/seeds/user.js
@@ -2,7 +2,8 @@ exports.seed = function(knex) {
 	return knex('user').del()
 		.then(function () {
 			return knex('user').insert([
-				{ name: 'admin', phone: '123345', height: 2, }
+				{ name: 'tes1', phone: '123345', height: 5, },
+				{ name: 'test2', phone: '54321', height: 10, },
 			]);
 		});
 };
