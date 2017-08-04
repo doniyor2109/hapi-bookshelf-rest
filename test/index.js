993628fd64cb11ec895f6434b9dcaaf801398bc4
doniyor <doniyor2109@mail.ru>
2017-08-04 11:26:39 +0500

    developing

--- test/index.js
+++ test/index.js
@@ -19,12 +19,32 @@ test.serial.cb(`GET /user`, function (t) {
             proxy: false,
             url: `${url}/user`,
             qs: {
-              order: 'phone,-name',
+              order: 'phone',
             },
         },
         function (err, response, body) {
           console.log(body);
             t.is(response.statusCode, 200, JSON.stringify(body));
+            t.end();
+        }
+    );
+});
+
+test.serial.cb(`POST /user`, function (t) {
+    request.post(
+        {
+            proxy: false,
+            url: `${url}/user`,
+            json: true,
+            body: {
+              phone: '99888777',
+              car: {
+                model: 'Jeep',
+              }
+            }
+        },
+        function (err, response, body) {
+            t.is(response.statusCode, 201, JSON.stringify(body));
             t.end();
         }
     );
