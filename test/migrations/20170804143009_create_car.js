d6cbc697f98e9647da1d6ff7f6a8e627ff904ff1
doniyor <doniyor2109@mail.ru>
2017-08-04 14:37:50 +0500

    added model field to car table

--- test/migrations/20170804143009_create_car.js
+++ test/migrations/20170804143009_create_car.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('car', function(table){
         table.increments('id').primary();
         table.string('name');
+        table.string('model');
     });
 };
 
