f43426ced3609eaa6927cbb7021972b0e7231b71
doniyor <doniyor2109@mail.ru>
2017-08-04 15:38:34 +0500

    fix: model relations

--- test/_model.js
+++ test/_model.js
@@ -1,34 +1,30 @@
 const joi = require('joi');
 
 module.exports = (bookshelfInstance) => {
-  let Car = bookshelfInstance.Model.extend({
-      tableName: 'car',
-      schema: {
-          name: joi.string(),
-          model: joi.string(),
-      },
-      user(){
-        user.relation = true;
+      let User = bookshelfInstance.Model.extend({
+          tableName: 'user',
+          schema: {
+              name: joi.string(),
+              phone: joi.string().required(),
+              height: joi.number(),
+              birth: joi.date(),
+              car_id: joi.number(),
+          },
+          car(){
+              return this.hasOne(Car, 'id', 'car_id');
+          }
+      });
 
-        return this.belongsTo(User);
-      }
-  });
+    let Car = bookshelfInstance.Model.extend({
+        tableName: 'car',
+        schema: {
+            name: joi.string(),
+            model: joi.string(),
+        },
+        user(){
+            return this.belongsTo(User, 'id', 'car_id');
+        }
+    });
 
-  function car() {
-    return this.hasOne(Car);
-  }
-  car.relation = true;
-
-  let User = bookshelfInstance.Model.extend({
-      tableName: 'user',
-      schema: {
-          name: joi.string(),
-          phone: joi.string().required(),
-          height: joi.number(),
-          birth: joi.date(),
-      },
-      car,
-  });
-
-  return User;
-}
+  return Car;
+};
