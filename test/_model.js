ae85e1d8260b97882fa61b3bac7c0c55f055e1a5
doniyor <doniyor2109@mail.ru>
2017-08-04 14:36:33 +0500

    Car model

--- test/_model.js
+++ test/_model.js
@@ -1,12 +1,34 @@
 const joi = require('joi');
 
 module.exports = (bookshelfInstance) => {
-  return bookshelfInstance.Model.extend({
+  let Car = bookshelfInstance.Model.extend({
+      tableName: 'car',
+      schema: {
+          name: joi.string(),
+          model: joi.string(),
+      },
+      user(){
+        user.relation = true;
+
+        return this.belongsTo(User);
+      }
+  });
+
+  function car() {
+    return this.hasOne(Car);
+  }
+  car.relation = true;
+
+  let User = bookshelfInstance.Model.extend({
       tableName: 'user',
       schema: {
           name: joi.string(),
+          phone: joi.string().required(),
           height: joi.number(),
           birth: joi.date(),
       },
+      car,
   });
+
+  return User;
 }
