84c2db22d9f43c9de25508259a9b249c8e7b44fe
doniyor <doniyor2109@mail.ru>
2017-08-06 23:24:13 +0500

    fix: static foreign keys

--- test/seeds/car_user.js
+++ test/seeds/car_user.js
@@ -1,21 +1,19 @@
 exports.seed = function (knex) {
-	return knex('user').del()
-		.then(function () {
+	return knex('user')
+		.del()
+		.then(() => knex('car').del())
+		.then(() => {
 			return knex('car')
 				.insert([
-					{ name: 'car1', model: 'model1'},
-					{ name: 'car1', model: 'model2'},
+					{ id: 1, name: 'car1', model: 'model1'},
+					{ id: 2, name: 'car1', model: 'model2'},
 				])
 				.returning('id');
-		}).then(function (ids) {
+		}).then((ids) => {
 			return knex('user')
-				.del()
-				.then(function () {
-					return knex('user')
-						.insert([
-							{ id: 1, name: 'user1', phone: 'phone2', height: 5, car_id: ids[0] - 1, },
-							{ id: 2, name: 'user2', phone: 'phone2', height: 10, car_id: ids[0], },
-						]);
-				});
+				.insert([
+					{ id: 1, name: 'user1', phone: 'phone2', height: 5, car_id: 1, },
+					{ id: 2, name: 'user2', phone: 'phone2', height: 10, car_id: 2, },
+				]);
 		});
 };
